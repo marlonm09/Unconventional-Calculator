4eed7b4e604b6caafa548e820c51073b8b8eb011
Marlon <montesino.marlon@gmail.com>
2022-08-02 15:12:42 -0600

    More string properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,6 @@ let currentResult = initialResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + initialResult + ' + 10) * 3 / 2 - 1';
+let calculationDescription = `(${initialResult} + 10) * 3 / 2 - 1`;
 
 outputResult (currentResult, calculationDescription);
