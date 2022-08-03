b6f8f81b14714643f8abda101808d1c7a1162e15
Marlon <montesino.marlon@gmail.com>
2022-08-03 09:47:57 -0600

    Added function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,13 @@
 const initialResult = 0;
-
 let currentResult = initialResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The result is ' + result);
+}
+
+add(1, 2);
+add(5, 5);
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
